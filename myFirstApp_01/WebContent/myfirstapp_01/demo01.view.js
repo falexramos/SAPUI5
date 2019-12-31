8c477d220d6b8a8fe37d130b4ad07beaa0880e95
favian <favian@DESKTOP-GBTGJ82>
2019-12-31 18:31:13 -0500

    mi primer app ui5 hola mundo basico

---
+++ myFirstApp_01/WebContent/myfirstapp_01/demo01.view.js
@@ -0,0 +1,24 @@
+sap.ui.jsview("myfirstapp_01.demo01", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf myfirstapp_01.demo01
+	*/ 
+	getControllerName : function() {
+		return "myfirstapp_01.demo01";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf myfirstapp_01.demo01
+	*/ 
+	createContent : function(oController) {
+ 		return new sap.m.Page({
+			title: "Hello Word !! Favian Ramos",
+			content: [
+			
+			]
+		});
+	}
+
+});
